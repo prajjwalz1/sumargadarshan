d4e2ae86ac53df6e5613ffed9caf42f7ada9b7b1
prajjwalacharya <acharyaprajjwol152@gmail.com>
2021-10-18 10:37:01 +0545

    heroku changes

--- sumargadarshan/sumargadarshan/settings.py
+++ sumargadarshan/sumargadarshan/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#p0(t(v!si1rzw$+uo5!f0od^%#2psx*+%+pdb1=6i$bux-b4='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
