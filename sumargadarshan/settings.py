cc25febc1937e4694236fda297c1ef6ee77a61b8
prajjwalacharya <acharyaprajjwol152@gmail.com>
2021-10-18 13:34:09 +0545

    debug set rto true again

--- sumargadarshan/settings.py
+++ sumargadarshan/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#p0(t(v!si1rzw$+uo5!f0od^%#2psx*+%+pdb1=6i$bux-b4='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
